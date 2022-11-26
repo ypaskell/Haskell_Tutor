1f76e853416d742c8d0fa1ee6b8bf52e6ab584e3
YiPing Pan <pan.yiping.fi@gmail.com>
2022-11-26 09:49:00 +0800

    Add new solution

--- 1_Introduction/05_Array_Of_N_Elements.hs
+++ 1_Introduction/05_Array_Of_N_Elements.hs
@@ -1,6 +1,10 @@
 -- https://www.hackerrank.com/challenges/fp-array-of-n-elements/problem
 
-fn n = show $ replicate n n
+-- old answer
+-- fn n = show $ replicate n n
+
+
+fn n = show [1..n]
 
 main = do
     n <- readLn :: IO Int
